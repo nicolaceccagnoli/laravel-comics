7e00c6312e6e8aa1a09e8e6a8ab433976a0eabc4
nicolaceccagnoli <nicola.ceccagnoli.94@gmail.com>
2024-02-23 14:46:06 +0100

    Header Completed

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -49,18 +49,30 @@
 @endphp
 
 <header>
-    <nav class="container g-0 py-3 d-flex">
-        <ul class="d-flex justify-content-around list-unstyled flex-grow-1 h-100 align-items-center">
-            @foreach ($links as $link)
-                <li class="h-100">
-                    <a href="{{ $link['url'] }}" class="text-decoration-none">
-                        {{ $link['label'] }}
-                    </a>
-                </li>
-            @endforeach
-        </ul>
-        <span class="d-inline">
-            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-        </span>
-    </nav>
+    <div class="header-top container">
+        <div class="row justify-content-end">
+            <div class="col-auto">
+                DC POWER VISA
+            </div>
+            <div class="col-auto">
+                ADDITIONAL DC SITES
+            </div>
+        </div>
+    </div>
+    <div class="header-bottom">
+        <nav class="container g-0 py-3 d-flex">
+            <ul class="d-flex justify-content-around list-unstyled flex-grow-1 h-100 align-items-center">
+                @foreach ($links as $link)
+                    <li class="h-100">
+                        <a href="{{ $link['url'] }}" class="text-decoration-none">
+                            {{ $link['label'] }}
+                        </a>
+                    </li>
+                @endforeach
+            </ul>
+            <span class="d-inline">
+                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+            </span>
+        </nav>
+    </div>
 </header>
